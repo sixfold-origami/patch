ab6e65fb1616f3cb0e0920e6b728bd7f1a386881
sixfold <rosepeck1997@gmail.com>
2025-05-04 13:00:47 -0700

    It plays the game (not well though)

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 
-use chess::Game;
+use chess::{ChessMove, EMPTY, Game, MoveGen};
+use rand::Rng;
 
 #[derive(Debug)]
 pub struct Engine {
@@ -9,16 +10,59 @@ pub struct Engine {
 }
 
 impl Engine {
-    /// Resets the board position to the starting position
-    pub fn starting_position(&mut self) {
+    /// Sets the debug flag
+    pub fn set_debug(&mut self, debug: bool) {
+        self.debug = debug;
+    }
+
+    /// Whether debugging is enabled
+    pub fn debug(&self) -> bool {
+        self.debug
+    }
+
+    /// Resets the internal state for a new game
+    pub fn reset_game(&mut self) {
         self.game = Game::new();
     }
 
+    /// Resets the board position to the starting position
+    pub fn set_starting_position(&mut self, moves: Vec<ChessMove>) {
+        self.game = Game::new();
+
+        moves.into_iter().for_each(|mv| {
+            self.game.make_move(mv);
+        });
+    }
+
     /// Resets the board to the given position
-    pub fn position(&mut self, fen: &str) -> Result<(), anyhow::Error> {
+    pub fn set_position(&mut self, fen: &str, moves: Vec<ChessMove>) -> Result<(), anyhow::Error> {
         self.game = Game::from_str(fen).map_err(|e| failure::Error::from(e).compat())?;
+        moves.into_iter().for_each(|mv| {
+            self.game.make_move(mv);
+        });
 
         Ok(())
+    }
+
+    pub fn best_move(&self) -> ChessMove {
+        let board = self.game.current_position();
+        let mut iter = MoveGen::new_legal(&board);
+
+        let targets = board.color_combined(!board.side_to_move());
+        iter.set_iterator_mask(*targets);
+
+        if let Some(mv) = iter.next() {
+            return mv;
+        }
+
+        iter.set_iterator_mask(!EMPTY);
+
+        let moves: Vec<_> = iter.collect();
+        if moves.is_empty() {
+            panic!("No legal moves");
+        }
+        let mut rng = rand::rng();
+        moves[rng.random_range(0..moves.len())]
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{error::Error, io::stdin};
 
+use engine::Engine;
 use vampirc_uci::{CommunicationDirection, UciMessage, parse_with_unknown};
 
-mod engine;
+pub mod engine;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut engine = Engine::default();
+
     for line in stdin().lines() {
         for m in parse_with_unknown(&line.unwrap())
             .into_iter()
@@ -18,25 +21,46 @@ fn main() -> Result<(), Box<dyn Error>> {
                     // Shake the nice GUI's hand
                     println!("{}", UciMessage::UciOk);
                 }
-                UciMessage::Debug(_) => unimplemented!(),
+                UciMessage::Debug(debug) => engine.set_debug(debug),
                 UciMessage::IsReady => {
-                    // Everything is blocking, so by the time we read this messages, we're ready
+                    // Everything is blocking, so by the time we read this message, we're ready
                     // TODO: make it so that it's not all blocking
                     println!("{}", UciMessage::ReadyOk);
                 }
                 UciMessage::Position {
                     startpos,
                     fen,
-                    moves: _moves,
-                } => {}
+                    moves,
+                } => {
+                    if startpos {
+                        engine.set_starting_position(moves);
+                    } else {
+                        engine.set_position(fen.unwrap().as_str(), moves)?;
+                    }
+                }
                 UciMessage::SetOption { .. } => unimplemented!(),
-                UciMessage::UciNewGame => unimplemented!(),
-                UciMessage::Stop => unimplemented!(),
+                UciMessage::UciNewGame | UciMessage::Stop => {
+                    // TODO: NOP for now
+                }
                 UciMessage::PonderHit => unimplemented!(),
-                UciMessage::Go { .. } => unimplemented!(),
+                UciMessage::Go {
+                    time_control: _time_control,
+                    search_control,
+                } => {
+                    if search_control.is_some() {
+                        unimplemented!()
+                    }
+
+                    // We're too stupid to do a real search, but the benefit is that we can respond right away :clueless:
+                    let mv = engine.best_move();
+                    println!("{}", UciMessage::best_move(mv));
+                }
                 UciMessage::Quit => return Ok(()),
                 UciMessage::Register { .. } => {
                     // We don't perform registration, so this is a NOP
+                }
+                UciMessage::Unknown(str, _) => {
+                    println!("Unknown UCI message: {}", UciMessage::info_string(str))
                 }
                 _ => {
                     // EngineToGui messages
