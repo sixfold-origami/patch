5ee753ea527d100c031ca65ab047956740273020
sixfold <rosepeck1997@gmail.com>
2025-05-05 10:48:47 -0700

    Add cutechess command

--- tools/self-play/src/main.rs
+++ tools/self-play/src/main.rs
@@ -32,6 +32,7 @@ fn main() -> Result<(), anyhow::Error> {
     let maseter_rev = get_rev();
     let mut handle = Command::new("cargo")
         .args(["build", "--release"])
+        .env("CARGO_TARGET_DIR", "./target/master")
         .spawn()
         .expect("Failed to start master build");
     let exit = handle.wait().expect("Build command is not running");
@@ -44,7 +45,24 @@ fn main() -> Result<(), anyhow::Error> {
         "Testing {} (experimental) against {} (master)",
         &experimental_rev, &maseter_rev
     );
-    // let mut handle = Command::new("D:\\Programs\\Cute Chess\\cutechess-cli.exe");
+    let mut handle = Command::new("D:\\Programs\\Cute Chess\\cutechess-cli.exe")
+        .args([
+            "-engine",
+            "cmd=./target/experimental/release/fold-chess.exe",
+            "-engine",
+            "cmd=./target/master/release/fold-chess.exe",
+            "-each",
+            "proto=uci",
+            "tc=40/60",
+            "-rounds",
+            "10",
+        ])
+        .spawn()
+        .expect("Failed to start cutechess command");
+    let exit = handle.wait().expect("cutechess command is not running");
+    if !exit.success() {
+        bail!("cutechess command failed");
+    }
 
     // Swap back to original branch
     let mut handle = Command::new("git")
