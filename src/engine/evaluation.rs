45a1eade0bba45804d2f7f1d6a9f2f6c0de3b618
sixfold <rosepeck1997@gmail.com>
2025-05-18 13:47:06 +0200

    Clean up unused eval function

--- src/engine/evaluation.rs
+++ src/engine/evaluation.rs
@@ -14,33 +14,6 @@ pub fn eval_heuristic(board: &Board) -> Score {
     Score::cp(score)
 }
 
-/// Scores the provided board based on pure material value, assuming that we are up to move
-#[allow(unused)]
-fn material_eval(board: &Board) -> i16 {
-    let mine = board.color_combined(board.side_to_move());
-    let theirs = board.color_combined(!board.side_to_move());
-
-    // Get pieces and do sums
-    let mut cp: i16 = 0;
-
-    cp += ((board.pieces(Piece::Pawn) & *mine).popcnt() * 100) as i16;
-    cp -= ((board.pieces(Piece::Pawn) & *theirs).popcnt() * 100) as i16;
-
-    cp += ((board.pieces(Piece::Knight) & *mine).popcnt() * 350) as i16;
-    cp -= ((board.pieces(Piece::Knight) & *theirs).popcnt() * 350) as i16;
-
-    cp += ((board.pieces(Piece::Bishop) & *mine).popcnt() * 350) as i16;
-    cp -= ((board.pieces(Piece::Bishop) & *theirs).popcnt() * 350) as i16;
-
-    cp += ((board.pieces(Piece::Rook) & *mine).popcnt() * 525) as i16;
-    cp -= ((board.pieces(Piece::Rook) & *theirs).popcnt() * 525) as i16;
-
-    cp += ((board.pieces(Piece::Queen) & *mine).popcnt() * 1000) as i16;
-    cp -= ((board.pieces(Piece::Queen) & *theirs).popcnt() * 1000) as i16;
-
-    cp
-}
-
 /// Scores the provided board using piece [`tables`]
 ///
 /// Always scored from the perspective of the player that is up to move
